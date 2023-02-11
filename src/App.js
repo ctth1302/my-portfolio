986dfdd43869f848b030ee1046bcdf7b8e5bbf17
ctth1302 <ctth1302@gmail.com>
2023-02-11 18:15:39 +1030

    Project set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='d-flex'>
+      <aside>
+        <div className='profile'>
+          <img src="/fav.png" alt="" />
+          <h1 className='text-center'>Hang Chung</h1>
+          <h3 className='text-center'>WEB DEVELOPER</h3>
+        </div>
+        <nav>
+
+        </nav>
+      </aside>
+      <section>
+        <section id='section-home'>
+
+        </section>
+        <section id='section-about'>
+
+        </section>
+        <section id='section-projects'>
+
+        </section>
+        <section id='section-contact'>
+
+        </section>
+      </section>
+    </main>
   );
 }
 
